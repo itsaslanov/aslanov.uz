4e5c43e4fd37039b9550bd692d3019fa7274ed34
Azizbek Aslanov <its.aslanov@gmail.com>
2022-05-12 17:34:01 +0500

    file updated

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.container');
+ScrollReveal().reveal('.container__header', { duration: 400 });
+ScrollReveal().reveal('.block', { delay: 400, interval: 600 });
+ScrollReveal().reveal('.container__footer ul li', { delay: 1500, interval: 200 });
